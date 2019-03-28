0e7955ea98bdf4fd8495d6de8286f2c9714d0157
psykepro <psykepro@abv.bg>
2019-03-28 14:29:21 +0200

    Adding Recipe model and renamed test_modules.py to test_models.py, because it was a typo

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -65,3 +65,14 @@ class ModuleTests(TestCase):
         )
 
         self.assertEqual(str(ingredient), ingredient.name)
+
+    def test_recipe_str(self):
+        """Test the Recipe string representation"""
+        recipe = models.Recipe.objects.create(
+            user=sample_user(),
+            title='Steak and mushroom sauce',
+            time_minutes=5,
+            price=5.00,
+        )
+
+        self.assertEqual(str(recipe), recipe.title)
