ce8a47d1bc678e34aa3d4776b3d3852bd0513911
psykepro <psykepro@abv.bg>
2019-03-28 11:34:34 +0200

    Added create ingredient feature.

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -20,18 +20,24 @@ class TagViewSet(viewsets.GenericViewSet,
         return self.queryset.filter(user=self.request.user).order_by('-name')
 
     def perform_create(self, serializer):
-        """Create a new tag"""
+        """Create a new Tag"""
         serializer.save(user=self.request.user)
 
 
 class IngredientViewSet(viewsets.GenericViewSet,
-                        mixins.ListModelMixin):
+                        mixins.ListModelMixin,
+                        mixins.CreateModelMixin):
     """Manage ingredients in the database"""
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
     queryset = Ingredient.objects.all()
+
     serializer_class = serializers.IngredientSerializer
 
     def get_queryset(self):
         """Return objects for the current authenticated user"""
         return self.queryset.filter(user=self.request.user).order_by('-name')
+
+    def perform_create(self, serializer):
+        """Create a new Ingredient"""
+        serializer.save(user=self.request.user)
